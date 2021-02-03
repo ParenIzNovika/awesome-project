e31db394413a2790fa600008a6c41cf4cc805b1f
ParenIzNovika <67006746+ParenIzNovika@users.noreply.github.com>
2021-02-03 12:56:37 +0300

    Add files via upload

---
+++ pr2_2/pr2_2/pr2_2.cpp
@@ -0,0 +1,16 @@
+// pr2_2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{int x,y;
+_asm
+{
+
+}
+
+	return 0;
+}
+
